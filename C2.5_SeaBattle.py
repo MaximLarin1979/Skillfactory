57c3ef0ed62cec4a739d8067524f5206efbe6ca5
Maxim_Larin <maks.larin@gmail.com>
2023-08-17 17:32:29 +0300

    Первая версия морского боя Версия 2, пытаюсь нарисовать доску Версия 3, попытка расстановки кораблей

--- C2.5_SeaBattle.py
+++ C2.5_SeaBattle.py
@@ -5,7 +5,6 @@ class Dot:
     ship_dot = "◼︎"
     destroyed_ship_dot = "X"
     missed_dot = "T"
-    column_dot = "|"
 
     def __init__(self, x, y):
         self.x = x
@@ -27,10 +26,10 @@ class Ship:
     def dots(self):
         if self.direction == 0:
             for i in range(self.size):
-                self.ship_dots = self.ship_dots + [Dot(self.x, self.y + i).ship_dot]
+                self.ship_dots = self.ship_dots + [Dot(self.x, self.y + i)]
         else:
             for i in range(self.size):
-                self.ship_dots = self.ship_dots + [Dot(self.x + i, self.y).ship_dot]
+                self.ship_dots = self.ship_dots + [Dot(self.x + i, self.y)]
         return self.ship_dots
 
 
@@ -38,50 +37,35 @@ class Board:
     def __init__(self, board=None, ships = None, hid = False, live_ships = None):
         if board is None:
             board = []
-        self.board = [[" " * 7] * 13]
+        self.board = [[Dot] * 6] * 6
         self.ships = ships
         self.hid = hid
         self.live_ships = live_ships
 
-    def empty_board(self):
-        for i in range(7): #генерируем нумератор строк
-            self.board[i][0] = i
-        for j in range(2, 13, 2): #генерируем нумератор столбцов
-                self.board[0][j] = i
-        for i in range(1, 7): #генерируем разделители ("|")
-            for j in range(1, 11, 2):
-                print([i], [j])
-                self.board[i][j] = Dot.column_dot
-        for i in range(7): #генерируем пустые точки
-            for j in range(2, 13, 2):
+    def generate_board(self):
+        for i in range(6): #генерируем точки
+            for j in range(6):
                 self.board[i][j] = Dot(i, j).empty_dot
         if self.hid == False:
-            for i in range(7):
-                for j in range(13):
-                    print(self.board[i][j], end=" ")
+            for i in range(6):
+                for j in range(6):
+                    print(self.board[i][j], end="")
                 print()
 
     def add_ship(self, ship_dots):
         for i in ship_dots:
-            self.board[i] = Dot.ship_dot
+            print(ship_dots[i].x)
         return self.board
 
 b = Board()
-b.empty_board()
+print(b.board)
+b.generate_board()
 
-# for i in range(7):
-#     for j in range(13):
-#         print(b.board[i][j], end=" ")
-#     print()
 
-b.empty_board()
+ship = Ship(3, int(input('x')), int(input('y')), int(input('направление')))
+print(ship.dots())
 
-# battleship = Ship(3, int(input('x')), int(input('y')), int(input('направление')))
-# print(battleship.dots())
-#
-#
-# b = Board.add_ship(battleship.dots)
-#
-# for i in range(7):
-#     for j in range(13):
-#         print(b.board[i][j], end=" ")
+b.add_ship(ship.dots())
+
+
+
