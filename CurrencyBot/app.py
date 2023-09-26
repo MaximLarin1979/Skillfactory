c2fae5445f80e5e1192692d39cb4d5205564a688
Maxim_Larin <maks.larin@gmail.com>
2023-09-26 18:25:45 +0300

    Версия 1. Начал писать бота в телеграм Версия 2. Все почти готово

--- CurrencyBot/app.py
+++ CurrencyBot/app.py
@@ -1,10 +1,8 @@
 import telebot
-
-TOKEN = ''
+from token_keys import TOKEN, keys
+from extensions import APIException, CurrencyConverter
 
 bot = telebot.TeleBot(TOKEN)
-
-keys = {'доллар': 'USD', 'евро': 'EUR', 'рубль': 'RUB'}
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -15,8 +13,23 @@ def bot_help(message: telebot.types.Message):
     bot.reply_to(message, text)
 
 
+@bot.message_handler(commands=['values'])
 def bot_values(message: telebot.types.Message):
     text = 'Доступные валюты:'
     for key in keys.keys():
-        text = '\n'.join(text, key, )
+        text = '\n'.join((text, key,))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def convert(message: telebot.types.Message):
+    values = message.text.split(' ')
+    if len(values) > 3:
+        raise APIException('Слишком много параметров')
+    quote, base, amount = values
+    result = CurrencyConverter.get_price(quote, base, amount)
+    text = f'Цена {amount} {quote} в {base} - {result}'
+    bot.send_message(message.chat.id, text)
+
+
+bot.polling(none_stop=True)
---
+++ CurrencyBot/token_keys.py
@@ -0,0 +1,2 @@
+TOKEN = '6661769336:AAFgW9vyvtayppV7rqY8bAVqSFGnOQIm7-E'
+keys = {'доллар': 'USD', 'евро': 'EUR', 'рубль': 'RUB'}
