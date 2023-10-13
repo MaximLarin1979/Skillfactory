36ebae327bd24246f0077c60f8743ebc82948197
Maxim_Larin <maks.larin@gmail.com>
2023-10-13 15:14:42 +0300

    1 версия GPT бота

--- S+GPT_bot/app.py
+++ S+GPT_bot/app.py
@@ -1,43 +1,66 @@
+import os
 import telebot
-from tokens import TOKEN
-from extensions import APIException, CurrencyConverter
+import openai
+import logging
+import time
+from tokens import TOKEN_TELEBOT, TOKEN_OPENAI
 
-bot = telebot.TeleBot(TOKEN)
+
+bot = telebot.TeleBot(TOKEN_TELEBOT)
+openai.api_key = TOKEN_OPENAI
+
+# логирование
+log_dir = os.path.join(os.path.dirname(__file__), 'ChatGPT_Logs')
+
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+
+logging.basicConfig(filename=os.path.join(log_dir, 'error.log'), level=logging.ERROR,
+                    format='%(levelname)s: %(asctime)s %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
 
+# Обработчик команд /start или /help
 @bot.message_handler(commands=['start', 'help'])
-def bot_help(message: telebot.types.Message):
-    text = 'Чтобы начать работу, введите команду боту в следующем формате:\n' \
-           '<имя переводимой валюты> <в какую валюту перевести> <количество переводимой валюты>\n' \
-           'Чтобы увидеть список доступных валют, введите команду "/values"'
-    bot.reply_to(message, text)
+def send_welcome(message):
+    bot.reply_to(message,
+                 'Привет!\nЯ ChatGPT 3.5 бот компании S+ консалтинг\U0001F916\nЗадай мне любой вопрос, и я постараюсь '
+                 'на него ответить!')
 
 
-@bot.message_handler(commands=['values'])
-def bot_values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
-    for key in keys.keys():
-        text = '\n'.join((text, key,))
-    bot.reply_to(message, text)
+# Функция генерации ответа используя OpenAI API
+def generate_response(prompt):
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return completion.choices[0].message.content
 
 
-@bot.message_handler(content_types=['text'])
-def convert(message: telebot.types.Message):
+# Обработчик команды /bot
+@bot.message_handler(commands=['bot'])
+def command_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.reply_to(message, text=response)
+
+
+# Обработчик остальных сообщений
+@bot.message_handler(func=lambda _: True)
+def handle_message(message):
+    prompt = message.text
+    response = generate_response(prompt)
+    bot.send_message(chat_id=message.from_user.id, text=response)
+
+
+# Запуск бота
+print('S+ ChatGPT бот запущен')
+
+while True:
     try:
-        values = message.text.split(' ')
-        if len(values) > 3:
-            raise APIException('Слишком много много параметров')
-        if len(values) < 3:
-            raise APIException('Слишком много мало параметров')
-        quote, base, amount = values
-        result = CurrencyConverter.get_price(quote, base, amount)
-    except APIException as e:
-        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
-    except Exception as e:
-        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
-    else:
-        text = f'Цена {amount} {quote} в {base} - {result}'
-        bot.send_message(message.chat.id, text)
-
-
-bot.polling(none_stop=True)
+        bot.polling(none_stop=True)
+    except (telebot.apihelper.ApiException, ConnectionError) as e:
+        logging.error(str(e))
+        time.sleep(5)
+        continue
