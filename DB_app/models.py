a3284d5ac4cff91f8b90c18ddfe732eff25282a8
Maxim_Larin <maks.larin@gmail.com>
2023-11-03 18:02:10 +0300

    Добавлены модели

--- DB_app/models.py
+++ DB_app/models.py
@@ -10,10 +10,15 @@ class Author(models.Model):
 
 
 class Category(models.Model):
+    category_name = models.CharField(max_length=255, unique=True)
     pass
 
 
 class Post(models.Model):
+    post_name = models.CharField(max_length=255)
+    post_text = models.CharField()
+    post_rating = models.FloatField(default=0.0)
+    post_time = models.DateTimeField(auto_now_add=True)
     pass
 
 
