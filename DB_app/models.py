caa0345bdccc21ff4b2eb39359233faaee5541b7
Maxim_Larin <maks.larin@gmail.com>
2023-11-07 16:15:00 +0300

    Добавлены модели

--- DB_app/models.py
+++ DB_app/models.py
@@ -4,27 +4,52 @@ from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    author_rating = models.IntegerField(default=0)
 
-    pass
+    def update_rating(self):
+        post_rating_sum = Post.objects.filter(author=self)
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=255, unique=True)
-    pass
 
 
 class Post(models.Model):
+    post_type_choices = (('ne', 'Новость'), ('ar', 'Статья'))
+    post_type = models.CharField(max_length=2, choices=post_type_choices)
     post_name = models.CharField(max_length=255)
     post_text = models.CharField()
-    post_rating = models.FloatField(default=0.0)
     post_time = models.DateTimeField(auto_now_add=True)
-    pass
+    post_rating = models.IntegerField(default=0)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, through='PostCategory')
+
+    def post_like(self):
+        self.post_rating += 1
+        self.save()
+
+    def post_dislike(self):
+        self.post_rating -= 1
+        self.save()
 
 
 class PostCategory(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    pass
+    comment_text = models.CharField()
+    comment_time = models.DateTimeField(auto_now_add=True)
+    comment_rating = models.IntegerField(default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def comment_like(self):
+        self.comment_rating += 1
+        self.save()
+
+    def comment_dislike(self):
+        self.comment_rating -= 1
+        self.save()
