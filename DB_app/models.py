f0b8a6c1d351bafb111aa7ef15b175c3d4c08583
Maxim_Larin <maks.larin@gmail.com>
2023-11-09 11:45:31 +0300

    Добавлены модели

--- DB_app/models.py
+++ DB_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db.models import Sum
 
 
 class Author(models.Model):
@@ -8,7 +8,13 @@ class Author(models.Model):
     author_rating = models.IntegerField(default=0)
 
     def update_rating(self):
-        post_rating_sum = Post.objects.filter(author=self)
+        post_rating_sum = Post.objects.filter(author=self).aggregate(Sum('post_rating'))['post_rating__sum']
+        comment_rating_sum = \
+            Comment.objects.filter(user__author=self).aggregate(Sum('comment_rating'))['comment_rating__sum']
+        post_comment_rating_sum = \
+            Comment.objects.filter(post__author=self).aggregate(Sum('comment_rating'))['comment_rating__sum']
+        self.author_rating = post_rating_sum * 3 + comment_rating_sum + post_comment_rating_sum
+        self.save()
 
 
 class Category(models.Model):
@@ -19,7 +25,7 @@ class Post(models.Model):
     post_type_choices = (('ne', 'Новость'), ('ar', 'Статья'))
     post_type = models.CharField(max_length=2, choices=post_type_choices)
     post_name = models.CharField(max_length=255)
-    post_text = models.CharField()
+    post_text = models.TextField()
     post_time = models.DateTimeField(auto_now_add=True)
     post_rating = models.IntegerField(default=0)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
@@ -33,6 +39,9 @@ class Post(models.Model):
         self.post_rating -= 1
         self.save()
 
+    def preview(self):
+        print(str(self.post_text)[:123], '...')
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
@@ -40,7 +49,7 @@ class PostCategory(models.Model):
 
 
 class Comment(models.Model):
-    comment_text = models.CharField()
+    comment_text = models.TextField()
     comment_time = models.DateTimeField(auto_now_add=True)
     comment_rating = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
